22ff6dc584b01070c2caf83b473aee0dee452d13
Joy Guevarra <joguevarra5@up.edu.ph>
2025-05-16 06:18:15 +0800

    chore: initialized cart page

---
+++ frontend/ALAN/src/pages/user/userCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../../components/Header'
+
+function UserCart() {
+    return (
+        <div>
+            <Header></Header>
+        </div>
+    )
+}
+
+export default UserCart
