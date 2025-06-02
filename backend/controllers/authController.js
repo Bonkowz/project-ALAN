696b21711a4cbb67490ef9addda1a66c4fc2e81b
Alan Vender <alanvenders@gmail.com>
2025-06-02 22:50:50 +0800

    chore: updated auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,96 +1,110 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import User from '../models/User.js';
+import User from "../models/User.js";
 
 // NOTE: POST
 export const register = async (req, res) => {
-    const { firstName, middleName, lastName, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ firstName, middleName, lastName, userType: "customer", email, password: hashedPassword });
-    try {
-        await newUser.save();
-        res.status(200).json({ message: "User registered successfully!" });
-        return;
-    } catch (err) {
-        res.status(500).json({ error: err });
-        return;
-    }
-}
+  const { firstName, middleName, lastName, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    firstName,
+    middleName,
+    lastName,
+    userType: "customer",
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(200).json({ message: "User registered successfully!" });
+    return;
+  } catch (err) {
+    res.status(500).json({ error: err });
+    return;
+  }
+};
 
 export const registerAdmin = async (req, res) => {
-    const { firstName, middleName, lastName, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ firstName, middleName, lastName, userType: "administrator", email, password: hashedPassword });
-    try {
-        await newUser.save();
-        res.status(200).json({ message: "Admin registered successfully!" });
-        return;
-    } catch (err) {
-        res.status(500).json({ error: err });
-        return;
-    }
-}
+  const { firstName, middleName, lastName, email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    firstName,
+    middleName,
+    lastName,
+    userType: "administrator",
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.status(200).json({ message: "Admin registered successfully!" });
+    return;
+  } catch (err) {
+    res.status(500).json({ error: err });
+    return;
+  }
+};
 
 export const login = async (req, res) => {
-    const email = req.body.email ? req.body.email.trim() : "";
-    const { password } = req.body;
-    const user = await User.findOne({ email });
-    try {
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-        }
-        jwt.sign(
-            {
-                firstName: user.firstName,
-                lastName: user.lastName,
-                userId: user._id,
-                email: user.email,
-                userType: user.userType
-            },
-            process.env.SECRET_KEY,
-            {}, 
-            (err, token) => {
-                if (err) throw err;
-                res
-                    .status(200)
-                    .cookie('token', token, {
-                        httpOnly: true,
-                        secure: true,
-                        sameSite: 'None',
-                        partitioned: true 
-                    }, (err))
-                    .json(user)
-            }
-        );
-    } catch (error) {
-        res.status(500).json({ error: 'Error logging in' });
+  const email = req.body.email ? req.body.email.trim() : "";
+  const { password } = req.body;
+  const user = await User.findOne({ email });
+  try {
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
     }
-}
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    jwt.sign(
+      {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userId: user._id,
+        email: user.email,
+        userType: user.userType,
+      },
+      process.env.SECRET_KEY,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res
+          .status(200)
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "None",
+            partitioned: true,
+          })
+          .json(user);
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ error: "Error logging in" });
+  }
+};
 
 // NOTE: GET
 export const getProfile = async (req, res) => {
-    const { token } = req.cookies;
-    if (token) {
-        jwt.verify(token, process.env.SECRET_KEY, {}, (err, user) => {
-            if (err) throw err;
-            res.status(200).json(user);
-        })
-    } else {
-        res.status(500).json(null);
-    }
-}
-
-// NOTE: proper handling breaks other code 
-export const logout = async (req, res) => {
-    res.clearCookie("token", { 
-        httpOnly: true, 
-        sameSite: 'None', 
-        secure: true,
-        partitioned: true 
+  const { token } = req.cookies;
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, {}, (err, user) => {
+      if (err) throw err;
+      res.status(200).json(user);
     });
-    res.status(200).json({ success: true, message: "Logged out successfully" });
+  } else {
+    res.status(500).json(null);
+  }
+};
+
+// NOTE: proper handling breaks other code
+export const logout = async (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    sameSite: "None",
+    secure: true,
+    partitioned: true,
+  });
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
